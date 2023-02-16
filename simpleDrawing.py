19d2cb9ee75af9605c7d35950f3d8d19c0073711
TRP64011655 <90514870+TRP64011655@users.noreply.github.com>
2023-02-16 18:02:35 +0700

    win2

--- simpleDrawing.py
+++ simpleDrawing.py
@@ -19,6 +19,7 @@ class Simple_drawing_window(QWidget):
     def draw(self,e):
         raise  
 
+
 class Simple_drawing_window1(Simple_drawing_window):
     def __init__(self):
         QWidget.__init__(self, None)
@@ -28,22 +29,6 @@ class Simple_drawing_window1(Simple_drawing_window):
     def paintEvent(self, e):
         p = QPainter()
         p.begin(self)
-
-        p.setPen(QColor(0, 0, 0))
-        p.setBrush(QColor(0, 127, 0))
-        p.drawPolygon([
-            QPoint(70, 100), QPoint(100, 110),
-            QPoint(130, 100), QPoint(100, 150),
-        ])
-
-        p.setPen(QColor(255, 127, 0))
-        p.setBrush(QColor(255, 127, 0))
-        p.drawPie(50, 150, 100, 100, 0, 180 * 16)
-
-        p.drawPolygon(
-            [QPoint(50, 200), QPoint(150, 200), QPoint(100, 400), ]
-        )
-
         p.drawPixmap(QRect(200, 100, 320, 320), self.pic_1)
         p.end()
 
