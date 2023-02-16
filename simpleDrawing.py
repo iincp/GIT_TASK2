c5e28840516f330f64bf1f37788a7a9c400df859
TRP64011655 <90514870+TRP64011655@users.noreply.github.com>
2023-02-16 18:20:02 +0700

    q1-1(done)

--- simpleDrawing.py
+++ simpleDrawing.py
@@ -15,10 +15,6 @@ class Simple_drawing_window(QWidget):
     def paintEvent(self, e): 
         pass
 
-    # abstract method
-    def draw(self,e):
-        raise  
-
 
 class Simple_drawing_window1(Simple_drawing_window):
     def __init__(self):
@@ -33,53 +29,12 @@ class Simple_drawing_window1(Simple_drawing_window):
         p.end()
 
 
-class Simple_drawing_window2(Simple_drawing_window):
-    def __init__(self) :
-        QWidget.__init__(self,None) 
-        self.setWindowTitle("Simple Drawing1")
-
-    def draw(self,n):  
-        p = QPainter()
-        p.begin(self)
-
-        p.setPen(QColor(0, 0, 0))
-        p.setBrush(QColor(0, 127, 0))
-        p.drawPolygon([
-            QPoint(70, 100), QPoint(100, 110),
-            QPoint(130, 100), QPoint(100, 150),
-        ])
-
-        p.setPen(QColor(255, 127, 0))
-        p.setBrush(QColor(255, 127, 0))
-        p.drawPie(50, 150,100, 100, 0, 180 * 16)
-
-        p.drawPolygon(
-            [QPoint(50, 200), QPoint(150, 200), QPoint(100, 400)]
-        )
-
-
-# derived class1
-class Simple_drawing_window1(Simple_drawing_window):
-    def __init__(self):
-        QWidget.__init__(self, None)
-        self.setWindowTitle("Simple Github Drawing1")
-
-    # overiden method
-    def paintEvent(self, e):
-        p = QPainter()
-        p.begin(self)
-        p.setPen(QColor(0, 0, 0))
-        p.setBrush(QColor(0, 127, 0))
-
-        #ur code
-
-        p.end()
-
 #derived class2
 class Simple_drawing_window2(Simple_drawing_window):
     def __init__(self):
         QWidget.__init__(self, None)
         self.setWindowTitle("Simple Github Drawing2")
+        self.rabbit = QPixmap("images/rabbit.png")
 
     #overiden method
     def paintEvent(self, e):
@@ -88,7 +43,7 @@ class Simple_drawing_window2(Simple_drawing_window):
         p.setPen(QColor(0, 0, 0))
         p.setBrush(QColor(0, 127, 0))
 
-        #ur code
+        p.drawPixmap(QRect(0, 0, 675, 475), self.rabbit)
 
         p.end()
 
@@ -97,6 +52,7 @@ class Simple_drawing_window3(Simple_drawing_window):
     def __init__(self):
         QWidget.__init__(self, None)
         self.setWindowTitle("Simple Github Drawing3")
+        self.pic_1 = QPixmap("images/cat.png")
 
     #overiden method
     def paintEvent(self, e):
@@ -105,7 +61,7 @@ class Simple_drawing_window3(Simple_drawing_window):
         p.setPen(QColor(0, 0, 0))
         p.setBrush(QColor(0, 127, 0))
 
-        #ur code
+        p.drawPixmap(QRect(0, 0, 675, 475), self.pic_1)
 
         p.end()
 
