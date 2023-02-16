e8be2bd96b2d3122b7067556ec132a331bab0b13
TRP64011655 <90514870+TRP64011655@users.noreply.github.com>
2023-02-16 17:02:25 +0700

    paintEvent(not done yet)

--- simpleDrawing.py
+++ simpleDrawing.py
@@ -9,3 +9,8 @@ class Simple_drawing_window(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
         self.setWindowTitle("Simple Github Drawing")
+        self.rabbit = QPixmap("images/rabbit.png")
+
+    def paintEvent(self, e):
+        p = QPainter()
+        p.begin(self)
