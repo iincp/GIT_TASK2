3745e796d8941ab63558404df1f55f2df8062b90
Fuku <90387760+fukuuuu@users.noreply.github.com>
2023-02-16 16:58:21 +0700

    add draft code1

---
+++ simpleDrawing.py
@@ -0,0 +1,11 @@
+# Hello Ice
+
+import sys
+from PySide6.QtCore import *
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+
+class Simple_drawing_window(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Simple Github Drawing")
